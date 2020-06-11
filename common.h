45a9fd4ed61059c346461e0def96cbe6def8f2a9
ws <2422523675@qq.com>
2020-06-11 09:17:05 +0800

    nothing

--- common.h
+++ common.h
@@ -24,22 +24,23 @@ namespace NovelBase {
         const QString reason_stored;
         const QByteArray charbuf;
     };
+
+    class ForeshadowConfig : public QDialog
+    {
+    public:
+        //                                      title,   keypath, modelindex
+        ForeshadowConfig(const QList<QPair<QString, QModelIndex> > &keystoryList, QWidget *parent=nullptr);
+
+        QDialog::DialogCode getForeshadowDescription(QModelIndex &index, QString &name, QString &desp0, QString &desp1);
+
+    private:
+        const QList<QPair<QString, QModelIndex> > &keystory_list;
+        QComboBox *const combox;
+        QLineEdit *const name_enter;
+        QTextEdit *const desp0, *const desp1;
+    };
+
 }
 
-
-class ForeshadowConfig : public QDialog
-{
-public:
-    //                                      title,   keypath, modelindex
-    ForeshadowConfig(const QList<QPair<QString, QModelIndex> > &keystoryList, QWidget *parent=nullptr);
-
-    QDialog::DialogCode getForeshadowDescription(QModelIndex &index, QString &name, QString &desp0, QString &desp1);
-
-private:
-    const QList<QPair<QString, QModelIndex> > &keystory_list;
-    QComboBox *const combox;
-    QLineEdit *const name_enter;
-    QTextEdit *const desp0, *const desp1;
-};
 
 #endif // COMMON_H
